afcfbd3f00f6af23349ff4105884f335bc64dc20
Takaaki Yumiba <52452416+taka1109y@users.noreply.github.com>
2021-10-26 19:17:25 +0900

    ルーム参加機能追加
    ルーム参加処理＆参加通知実装済
    本体クライアントの画面遷移機能未実装

--- src/server.js
+++ src/server.js
@@ -9,6 +9,9 @@ const handleListen = () => console.log(`Listening on http://localhost:3000`);
 const httpServer = http.createServer(app);
 const wsServer = new Server(httpServer);
 
+let userList = [];
+let users = [];
+
 // 開発サーバの駆動は「npm run dev」で！
 // 下のブロックから機能実装！
 wsServer.on("connection", (socket) => {
@@ -17,6 +20,35 @@ wsServer.on("connection", (socket) => {
   //    処理...
   // });
   //
+  //ルーム参加処理
+  socket.on('join-room', function({username, roomid, ipaddress, language}, callback) {
+    try{
+      //接続中のクライアントのIPアドレスのチェック
+      if(socket.client.conn.remoteAddress == ipaddress){
+        userList.push({username});
+        users.push({
+            socketid: socket.id, 
+            ipaddres: ipaddress, 
+            roomid: roomid,
+            username: username,
+            language: language
+        });
+        socket.join(roomid);
+        callback();
+        //ユーザー参加通知のemit処理
+        io.sockets.emit('join-room-effect', userList);
+        //本体クライアントの入室時画面切り替え処理
+
+      }else{
+          //ない場合は決めていない
+          //テスト用に例外エラーを生成
+          throw new error();
+      }
+    }catch(e){
+        console.error(e.message);
+    }
+  });
+
   //// ソケットのエミット例
   // socket.to("受信するユーザまたは会議ルーム").emit("イベント名", (arg1, arg2, ...) => {
   //    処理...
