bbb91ef0c47fbeb4e676a7e0c0dc7d13f1817492
Minho Yun <minhoyun.dev@gmail.com>
2021-12-02 18:03:04 +0900

    deploy

--- build/server.js
+++ build/server.js
@@ -2,8 +2,6 @@
 
 var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
 
-var _typeof = require("@babel/runtime/helpers/typeof");
-
 var _regenerator = _interopRequireDefault(require("@babel/runtime/regenerator"));
 
 var _slicedToArray2 = _interopRequireDefault(require("@babel/runtime/helpers/slicedToArray"));
@@ -24,12 +22,6 @@ var _constants = require("./constants");
 
 var _utils = require("./utils");
 
-var credentials = _interopRequireWildcard(require("./translateSettings"));
-
-function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "function") return null; var cacheBabelInterop = new WeakMap(); var cacheNodeInterop = new WeakMap(); return (_getRequireWildcardCache = function _getRequireWildcardCache(nodeInterop) { return nodeInterop ? cacheNodeInterop : cacheBabelInterop; })(nodeInterop); }
-
-function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { "default": obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj["default"] = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
-
 require("dotenv").config();
 
 var PORT = process.env.PORT || 4000;
@@ -41,8 +33,19 @@ app.use(_express["default"].urlencoded({
 }));
 var db = new _database.Database();
 var translate = new _v.Translate({
-  credentials: credentials,
-  projectId: credentials.project_id
+  credentials: {
+    type: "service_account",
+    project_id: process.env.PROJECT_ID,
+    private_key_id: process.env.TRANSLATOR_KEY_ID,
+    private_key: process.env.TRANSLATOR_KEY,
+    client_email: process.env.CLIENT_EMAIL,
+    client_id: process.env.CLIENT_ID,
+    auth_uri: "https://accounts.google.com/o/oauth2/auth",
+    token_uri: "https://oauth2.googleapis.com/token",
+    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+    client_x509_cert_url: process.env.CLIENT_CERT
+  },
+  projectId: process.env.PROJECT_ID
 }); // 翻訳処理
 
 var translateText = /*#__PURE__*/function () {
